d9978372c3f74d7f7f9d43559416a099149ed295
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-27 15:05:38 -0500

    easy fix done

--- src/Author.java
+++ src/Author.java
@@ -4,9 +4,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
-
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
@@ -28,7 +28,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+//    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
